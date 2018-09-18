ffb0bdfa0245b2daabdab1b04965e0a0e92526d5
Faip Mardoni <faipmardoni@gmail.com>
2018-09-18 14:36:16 +0700

    add undo redo

--- es/index.js
+++ es/index.js
@@ -2,6 +2,7 @@ import Diagram, { store } from './diagram/component';
 import { setEntities, setCustom, addEntity,setError, setNotError, removeID, addScroll } from './entity/reducer';
 import { setConfig } from './config/reducer';
 import { minHeightCanvas, zoom } from './canvas/reducer';
+import { undo, redo } from './history/reducer';
 import diagramOn from './diagramOn/';
 
-export { Diagram, diagramOn, store, setEntities, setConfig, setCustom, addEntity, setError, setNotError, removeID, addScroll, minHeightCanvas, zoom };
+export { Diagram, diagramOn, store, setEntities, setConfig, setCustom, addEntity, setError, setNotError, removeID, addScroll, minHeightCanvas, zoom, undo, redo };
